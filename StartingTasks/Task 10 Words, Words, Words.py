8be9f745b39aabe6ea6cc71afc6d2595fc61dc31
dracstorey <dracstorey@gmail.com>
2021-09-15 12:09:10 +0100

    Task 10 completed

--- StartingTasks/Task 10 Words, Words, Words.py
+++ StartingTasks/Task 10 Words, Words, Words.py
@@ -1,2 +1,34 @@
+
+## Task 10. Print how manyb words are in a sentence.
+## User inpyts a sentence
 sentence = input("Input a sentence") 
-print(sentence.count(" ")+1)
+
+# Test sentence
+#sentence = "Hello Dr Kinch how   are you   today.  "
+
+# Flag to record if the character is a space or not. Setbto Tru so first character can be counted
+s_flag = True
+
+# Number of words counted (final output)
+words = 0
+
+# Loop counter
+counter = 0
+
+# Iterate through each letter of the sentence
+while counter < len(sentence) - 1:
+    # If we encounter a space set space flag to true
+    if sentence[counter] == " ":
+        s_flag = True
+    else:
+        # If space flag is true and character is not a space then new word begins
+        if s_flag == True:
+            # Increae number of words by 1
+            words = words + 1
+            # set spave flag to false so we don't count anorther word if next character is a letter
+            s_flag = False
+    # increment the loop counter
+    counter = counter + 1
+
+
+print("There are", words, " words in this sentence")
