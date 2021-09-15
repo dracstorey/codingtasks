43c4ef42925b231445b6ac9db008dd56d1431ea9
dracstorey <dracstorey@gmail.com>
2021-09-15 10:35:00 +0100

    Task 9 completed

--- StartingTasks/Task 9 Comparison of Three.py
+++ StartingTasks/Task 9 Comparison of Three.py
@@ -1,15 +1,30 @@
-number1 = int(input("Input a number"))
-number2 = int(input("Input a second number"))
-number3 = int(input("Input a third number"))
-if number1 > number2 and number1> number3 and number2 > number3:
-    print (number1, number2, number3)
-elif number1 > number2 and number1> number3 and number3 > number2:
-    print (number1, number3, number2)
-elif number2 > number1 and number2> number3 and number3 > number1:
-    print (number2, number3, number1)
-elif number2 > number1 and number2> number3 and number1 > number3:
-    print (number2, number1, number3)
-elif number3 > number1 and number3> number1 and number2 > number1:
-    print (number3, number2, number1)
-elif number3 > number1 and number3> number1 and number1 > number2:
-    print (number3, number1, number2)
+## Task 9 - input 3 numbers then output in order
+
+# User inputs 3 nunbers
+a = int(input("Input a number"))
+b = int(input("Input a second number"))
+c = int(input("Input a third number"))
+
+# check a and b and swap if a is larger
+if a > b:
+    # swap a and b over
+    temp = a
+    a = b
+    b = temp
+
+# Check a against c and swap if larger
+if a > c:
+    # swap a and c over
+    temp = a
+    a = c
+    c = temp
+
+# smallest is now in posiiton a so check b against c
+if b > c:
+    # swap ba nd c over
+    temp = b
+    b = c
+    c = temp
+
+ ## Print in order   
+print (a, "<", b, "<", c)
