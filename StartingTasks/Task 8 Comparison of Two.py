3b8c12b982781bd3f7a815addf58d61616d2787d
dracstorey <dracstorey@gmail.com>
2021-09-15 09:20:55 +0100

    Task 8 done

--- StartingTasks/Task 8 Comparison of Two.py
+++ StartingTasks/Task 8 Comparison of Two.py
@@ -1,6 +1,14 @@
+## Task 8 - Compare two number print out larger then smaller
+
+# User inputs two numbers
 number1 = int(input("Input a number"))
 number2 = int(input("Input a second number"))
+
+# Check if number1 is larger
 if number1 > number2:
+    # if number 1 larger print numbers1 then number 2
     print (number1, number2)
 else:
+    # print numer2 then number 1
     print(number2, number1)
+#end if
