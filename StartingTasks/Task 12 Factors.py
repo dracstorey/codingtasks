e6db3866f1aeef48d20e149a8c3157d232b66ed8
dracstorey <dracstorey@gmail.com>
2021-09-15 16:19:45 +0100

    Task 12 - completed

--- StartingTasks/Task 12 Factors.py
+++ StartingTasks/Task 12 Factors.py
@@ -1,13 +1,23 @@
-n = 1
+## Print out the factors of a number
 
-x = int(input("Input an integer"))
-print("The factors of ", x, "are: ")
-while n <= x:
-    if x%n == 0:
-        print(n)
+
+
+# Get the user input and covert to integer
+number = int(input("Input an integer"))
+
+counter = 1
+# Print first part of the statement
+print("The factors of ", number, "are: ")
+
+# Loop round all number between 1 and half of the given number
+while counter < number // 2 +1:
+    
+    # Check if it divides .. remainder will be zero
+    if number%counter == 0:
+        # Print the divisor
+        print(counter)
     #endif
-    n=n+1
+   
+   # increase the counter
+    counter = counter + 1
 #endwhile
-
-
-
